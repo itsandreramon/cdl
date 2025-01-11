5bdefb06d2694de62127cf0e7f9b0a381aec66fa
itsandreramon <andre.ramon.thiele@icloud.com>
2025-01-11 11:17:00 +0100

    init

---
+++ shared/src/commonMain/kotlin/co/andrethiele/cdl/Greeting.kt
@@ -0,0 +1,9 @@
+package co.andrethiele.cdl
+
+class Greeting {
+    private val platform: Platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
---
+++ shared/src/commonMain/kotlin/co/andrethiele/cdl/Platform.kt
@@ -0,0 +1,7 @@
+package co.andrethiele.cdl
+
+interface Platform {
+    val name: String
+}
+
+expect fun getPlatform(): Platform
