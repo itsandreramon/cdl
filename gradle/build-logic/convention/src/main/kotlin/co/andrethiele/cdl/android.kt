2b93116fec0bc62ad0b3a2d5f9ed3fea18264c0d
itsandreramon <andre.ramon.thiele@icloud.com>
2025-01-13 11:05:18 +0100

    set all warnings as errors

--- gradle/build-logic/convention/src/main/kotlin/co/andrethiele/cdl/android.kt
+++ gradle/build-logic/convention/src/main/kotlin/co/andrethiele/cdl/android.kt
@@ -4,7 +4,7 @@ import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 
-fun Project.configureAndroid(extension: CommonExtension<*, *, * , *, *, *>) {
+fun Project.configureAndroid(extension: CommonExtension<*, *, *, *, *, *>) {
   extension.apply {
     compileSdk = Versions.COMPILE_SDK
     defaultConfig { minSdk = Versions.MIN_SDK }
@@ -14,6 +14,4 @@ fun Project.configureAndroid(extension: CommonExtension<*, *, * , *, *, *>) {
       sourceCompatibility = JavaVersion.toVersion(Versions.JVM_TARGET)
     }
   }
-
-  configureKotlin()
 }
--- gradle/build-logic/convention/src/main/kotlin/co/andrethiele/cdl/kotlin.kt
+++ gradle/build-logic/convention/src/main/kotlin/co/andrethiele/cdl/kotlin.kt
@@ -3,15 +3,44 @@ package co.andrethiele.cdl
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaPluginExtension
 import org.gradle.jvm.toolchain.JavaLanguageVersion
+import org.gradle.kotlin.dsl.assign
 import org.gradle.kotlin.dsl.configure
+import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
+import org.jetbrains.kotlin.gradle.dsl.KotlinCommonCompilerOptions
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+import org.jetbrains.kotlin.gradle.dsl.KotlinProjectExtension
 
 private fun Project.java(action: JavaPluginExtension.() -> Unit) =
   extensions.configure<JavaPluginExtension>(action)
+
+private fun Project.kotlin(action: KotlinProjectExtension.() -> Unit) =
+  extensions.configure<KotlinProjectExtension>(action)
 
 private fun Project.configureJava() {
   java { toolchain { languageVersion.set(JavaLanguageVersion.of(Versions.JVM_TARGET)) } }
 }
 
+private fun KotlinProjectExtension.compilerOptions(block: KotlinCommonCompilerOptions.() -> Unit) {
+  when (this) {
+    is KotlinJvmProjectExtension -> compilerOptions.block()
+    is KotlinAndroidProjectExtension -> compilerOptions.block()
+    is KotlinMultiplatformExtension -> {
+      targets.all {
+        compilations.all { compileTaskProvider.configure { compilerOptions { block() } } }
+      }
+    }
+
+    else -> error("Unknown kotlin extension $this")
+  }
+}
+
 fun Project.configureKotlin() {
   configureJava()
+  kotlin {
+    compilerOptions {
+      allWarningsAsErrors = true
+      freeCompilerArgs.add("-Xexpect-actual-classes")
+    }
+  }
 }
